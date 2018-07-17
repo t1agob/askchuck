8b8c1b635ecaf50c5b4a01173e950a226753f294
Tiago Barbosa <tbarbos@hotmail.com>
2018-07-17 18:27:20 +0100

    added askchuck.web project

---
+++ askchuck.web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using askchuck.web.Models;
+
+namespace chuck.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HttpClient client = new HttpClient();
+
+        public IActionResult Index()
+        {
+            ViewBag.Fact = "Just ask Chuck!";
+
+            return View();
+        }
+
+        public async Task<IActionResult> RandomFact()
+        {
+            HttpResponseMessage response = await client.GetAsync("http://chuckapi/api/quotes");
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Fact = await response.Content.ReadAsStringAsync();
+            }
+
+
+
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
